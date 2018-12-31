9e475484a2f71923ad01b50be9fa7aa560bb714e
ChandaranaNamita <namita.chandarana@umusic.com>
2018-12-31 16:16:24 +0000

    Adding selenium steps

--- src/main/java/CreateAccount.java
+++ src/main/java/CreateAccount.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CreateAccount {
-@Test
-    public void signUp(){
+    @Test
+    public void signUp() {
     /*
 
 Scenario: Create a new account
@@ -16,11 +17,16 @@ And my username is displayed
 Tip: do not put underscores in the username
 
      */
-    System.setProperty("webdriver.gecko.driver", "C:/Users/ChandaN/Desktop/geckodriver-v0.23.0-win64/geckodriver.exe");
-    WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.gecko.driver", "C:/Users/ChandaN/Desktop/geckodriver-v0.23.0-win64/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://en-gb.facebook.com/");
         driver.getTitle();
+        driver.findElement(By.xpath("//*[@name='firstname']")).sendKeys("SQS");
+        driver.findElement(By.xpath("//*[@name='lastname']")).sendKeys("Global");
+        driver.findElement(By.xpath("//*[@name='reg_email__']")).sendKeys("namita.chandarana@sqs.com");
+        driver.findElement(By.xpath("//*[@name='reg_email_confirmation__']")).sendKeys("namita.chandarana@sqs.com");
+        driver.findElement(By.xpath("//*[@name='reg_passwd__']")).sendKeys("PasswordUnitedKingdom311218");
+//    driver.findElement(By.id("username")).sendKeys("hello");
+        //   driver.findElement(By.id("username")).sendKeys("hello");
     }
-
-
 }
